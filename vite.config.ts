3666889de183ca66f2e4e6ed5b43a04e67a6f794
Guja <rnwkgur7@gmail.com>
2026-01-08 12:01:16 +0800

    Fix vite config (mode typing)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }: ConfigEnv) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === "gh" ? "/ganada/" : "/",
 }));
