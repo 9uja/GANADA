bc9b6fba1f2b45e2ba88cc21eb428f9b0d8ff674
Guja <rnwkgur7@gmail.com>
2026-01-08 11:57:33 +0800

    Fix vite config mode + env base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }: ConfigEnv) => ({
   plugins: [react()],
-  // Netlify/로컬: /
-  // GitHub Pages(레포명 menu): /menu/
-  base: mode === "gh" ? "/menu/" : "/",
+  base: mode === "gh" ? "/ganada/" : "/",
 }));
